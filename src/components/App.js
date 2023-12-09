624894ecd48225ec07733ff2dc5c32367ac7c8e9
Tuğcan Özaktaş <tugcanozaktas@hotmail.com>
2023-12-09 16:24:25 +0200

    moved the files into seperate folders to tidy up the project

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import PropTypes from "prop-types";
+import React from "react";
+import "../styles/App.css";
+import LocationDetails from "./LocationDetails";
+
+function App({ location }) {
+  const { city, country } = location;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <LocationDetails city={city} country={country} />
+    </div>
+  );
+}
+
+App.propTypes = {
+  location: PropTypes.shape({
+    city: PropTypes.string,
+    country: PropTypes.string,
+  }).isRequired,
+};
+
+export default App;
